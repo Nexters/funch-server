9845b6e5f2306bcbc7f87550026040a39fe8ed19
yeonnseok <ljinsk3@gmail.com>
2024-01-20 16:38:08 +0900

    feat: ktlint

--- api/src/test/kotlin/kr/co/funch/api/domain/member/MemberServiceTest.kt
+++ api/src/test/kotlin/kr/co/funch/api/domain/member/MemberServiceTest.kt
@@ -1,8 +1,6 @@
 package kr.co.funch.api.domain.member
 
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -12,15 +10,17 @@ import reactor.kotlin.core.publisher.toMono
 class MemberServiceTest : BehaviorSpec() {
     init {
         val memberRepository: MemberRepository = mockk()
-        val sut = MemberService(
-            memberRepository = memberRepository,
-        )
+        val sut =
+            MemberService(
+                memberRepository = memberRepository,
+            )
 
         Given("MemberService") {
-            coEvery { memberRepository.findMember(any()) } returns Member(
-                id = "5f9f1b9b9d6b9e1b9d6b9e1b",
-                name = "test",
-            ).toMono()
+            coEvery { memberRepository.findMember(any()) } returns
+                Member(
+                    id = "5f9f1b9b9d6b9e1b9d6b9e1b",
+                    name = "test",
+                ).toMono()
 
             When("findMember") {
                 val member = sut.findMember("5f9f1b9b9d6b9e1b9d6b9e1b")
