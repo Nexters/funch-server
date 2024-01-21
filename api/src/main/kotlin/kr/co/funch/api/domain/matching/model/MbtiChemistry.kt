2f46c66edf95c1fce9a5f72e88cf6be6ce6ae42f
seyeong <seyoung7555@naver.com>
2024-01-21 23:56:39 +0900

    Mbti 궁합 정보 저장할 도메인 및 repository 생성

---
+++ api/src/main/kotlin/kr/co/funch/api/domain/matching/model/MbtiChemistry.kt
@@ -0,0 +1,17 @@
+package kr.co.funch.api.domain.matching.model
+
+import kr.co.funch.api.domain.member.model.Mbti
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "MbtiChemistry")
+data class MbtiChemistry(
+    @Id
+    val id: MbtiChemistryId,
+    val message: String,
+) {
+    data class MbtiChemistryId(
+        val referenceMbti: Mbti,
+        val targetMbti: Mbti,
+    )
+}
