7881e22286ea8712f1efc4f0c8a7c2c16a831466
seyeong <seyoung7555@naver.com>
2024-01-28 01:38:20 +0900

    멤버 매칭 서비스 개발

--- api/src/main/kotlin/kr/co/funch/api/domain/matching/MemberMatchingService.kt
+++ api/src/main/kotlin/kr/co/funch/api/domain/matching/MemberMatchingService.kt
@@ -1,49 +1,38 @@
 package kr.co.funch.api.domain.matching
 
 import kr.co.funch.api.domain.matching.model.ConstellationChemistry
-import kr.co.funch.api.domain.matching.model.MatchingClubInfo
 import kr.co.funch.api.domain.matching.model.MbtiChemistry
 import kr.co.funch.api.domain.matching.model.MemberMatching
-import kr.co.funch.api.domain.matching.model.SubwayMatchingInfo
 import kr.co.funch.api.domain.member.MemberService
-import kr.co.funch.api.domain.member.model.Constellation
 import org.springframework.stereotype.Service
 
 @Service
 class MemberMatchingService(
     private val memberService: MemberService,
-    private val constellationChemistryRepository: ConstellationChemistryRepository,
 ) {
     suspend fun getMatchingInfo(
         requestMemberId: String,
         targetMemberCode: String,
     ): MemberMatching {
         val requestMember = memberService.findMember(requestMemberId)
-        // TODO: 임시로 매칭 key로 device code 사용, 매칭 시 사용하는 코드 논의 후 변경
-        val matchingTargetMember = memberService.findMemberByDeviceNumber(targetMemberCode)
+        val targetMember = memberService.findMemberByDeviceNumber(targetMemberCode)
 
-        val mbtiChemistry = MbtiChemistry.of(requestMember.mbti, matchingTargetMember.mbti)
+        val mbtiChemistry = MbtiChemistry.of(requestMember.mbti, targetMember.mbti)
         val constellationChemistry =
-            findConstellationChemistry(requestMember.constellation, matchingTargetMember.constellation)
-        val matchingClubInfo = MatchingClubInfo.of(requestMember.clubs, matchingTargetMember.clubs)
+            ConstellationChemistry.of(
+                requestMember.constellation,
+                targetMember.constellation,
+            )
+        val matchedClubs = requestMember.getMatchedClubs(targetMember)
+        val matchedSubwayStations = requestMember.getMatchedSubwayStations(targetMember)
 
         return MemberMatching(
+            targetMember,
             mbtiChemistry,
             constellationChemistry,
-            requestMember.jobGroup == matchingTargetMember.jobGroup,
-            matchingClubInfo,
-            SubwayMatchingInfo("싱싱미역"), // TODO: SubwayStation document로부터 불러와서 비교 후 MatcingInfo화
+            requestMember.hasSameJobGroup(targetMember),
+            matchedClubs,
+            matchedSubwayStations,
         )
     }
-
-    private suspend fun findConstellationChemistry(
-        referenceConstellation: Constellation,
-        targetConstellation: Constellation,
-    ): ConstellationChemistry =
-        constellationChemistryRepository.findConstellationByid(
-            ConstellationChemistry.ConstellationChemistryId(referenceConstellation, targetConstellation),
-        )
-            ?: throw IllegalArgumentException(
-                "Constellation not found - reference : $referenceConstellation, target : $targetConstellation",
-            )
 }
