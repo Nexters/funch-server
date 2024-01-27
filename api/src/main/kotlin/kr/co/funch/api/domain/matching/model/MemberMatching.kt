112cc69f399013b0e0729208e93972a83565e694
seyeong <seyoung7555@naver.com>
2024-01-28 00:47:00 +0900

    MemberMatching에 상대 멤버 정보 필드 추가

--- api/src/main/kotlin/kr/co/funch/api/domain/matching/model/MemberMatching.kt
+++ api/src/main/kotlin/kr/co/funch/api/domain/matching/model/MemberMatching.kt
@@ -1,9 +1,11 @@
 package kr.co.funch.api.domain.matching.model
 
 import kr.co.funch.api.domain.member.model.Club
+import kr.co.funch.api.domain.member.model.Member
 import kr.co.funch.api.domain.member.model.SubwayStation
 
 data class MemberMatching(
+    val targetMember: Member,
     val mbtiChemistry: MbtiChemistry,
     val constellationChemistry: ConstellationChemistry,
     val jobMatching: Boolean,
@@ -22,7 +24,7 @@ data class MemberMatching(
         return matchedItem * 100 / totalItem
     }
 
-    private fun addMatchedItemIf(condition: Boolean) {
+    private fun countMatchedItemIf(condition: Boolean) {
         totalItem += 1
         if (condition) {
             matchedItem += 1
