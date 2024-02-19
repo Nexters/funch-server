012ecc48cce7ba9e267c0e9ab11bb2093597193b
seyeong <seyoung7555@naver.com>
2024-02-19 23:56:25 +0900

    list -> set 반영

--- api/src/main/kotlin/kr/co/funch/api/interfaces/dto/SubwayStationDto.kt
+++ api/src/main/kotlin/kr/co/funch/api/interfaces/dto/SubwayStationDto.kt
@@ -19,7 +19,7 @@ object SubwayStationDto {
                 return SubwayStationResponse(
                     id = subwayStation.id.toString(),
                     name = subwayStation.name,
-                    lines = subwayStation.lines.map { SubwayStation.SubwayLine.valueOf(it) },
+                    lines = subwayStation.lines.toList(),
                     location =
                         Location(
                             latitude = subwayStation.location.latitude.toString(),
