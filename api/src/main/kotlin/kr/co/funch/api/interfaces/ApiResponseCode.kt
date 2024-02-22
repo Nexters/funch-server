cc3bc72ce14c9048089b21acaa15702ba1397c7d
seyeong <seyoung7555@naver.com>
2024-02-22 23:53:59 +0900

    ApiResponseCode 정의

---
+++ api/src/main/kotlin/kr/co/funch/api/interfaces/ApiResponseCode.kt
@@ -0,0 +1,21 @@
+package kr.co.funch.api.interfaces
+
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class ApiResponseCode(
+    val code: String,
+    val description: String,
+) {
+    SUCCESS("1000", "성공"),
+    INVALID_INPUT("2000", "입력값 오류"),
+    UNAUTHORIZED("3000", "인증 오류"),
+    FAIL("4000", "실패"),
+    MATCHING_PROFILE_NOT_EXIST("4001", "매칭상대 프로필 존재하지 않음"),
+    INTERNAL_ERROR("5000", "서버 오류"),
+    ;
+
+    @JsonValue
+    fun getCode(): String {
+        return code
+    }
+}
