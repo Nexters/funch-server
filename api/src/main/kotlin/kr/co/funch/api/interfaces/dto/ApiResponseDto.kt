7f9b37e4e0adf166928e1f00dffc46f7c241a4af
seyeong <seyoung7555@naver.com>
2024-02-22 23:54:14 +0900

    ApiResponseDto 기본값 정의
    - 성공 기준으로 세팅

--- api/src/main/kotlin/kr/co/funch/api/interfaces/dto/ApiResponseDto.kt
+++ api/src/main/kotlin/kr/co/funch/api/interfaces/dto/ApiResponseDto.kt
@@ -1,7 +1,11 @@
 package kr.co.funch.api.interfaces.dto
 
+import kr.co.funch.api.interfaces.ApiResponseCode
+import org.springframework.http.HttpStatus
+
 data class ApiResponseDto<T>(
-    val status: String,
-    val message: String?,
-    val data: T?,
+    val status: String = HttpStatus.OK.value().toString(),
+    val message: String? = HttpStatus.OK.reasonPhrase,
+    val code: ApiResponseCode = ApiResponseCode.SUCCESS,
+    val data: T? = null,
 )
